97521adf11f3a2c1038070a99dbaeafd849f7b4b
Vadim <92179377+Zhuzhalica@users.noreply.github.com>
2024-04-02 23:34:01 +0500

    add checlstyle

--- src/main/java/org/example/project/config/Config.java
+++ src/main/java/org/example/project/config/Config.java
@@ -1,23 +1,16 @@
 package org.example.project.config;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
-import lombok.RequiredArgsConstructor;
+
 import org.example.project.settings.ProjectSettings;
 import org.example.project.utils.JWTUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.zalando.problem.violations.ConstraintViolationProblemModule;
 
-import java.util.List;
 import java.util.Set;
 
 @Configuration
